c61eddd89f372120c3776326468ac77f5657e630
Diogo Mateus <diogo.tpr@gmail.com>
2022-12-27 17:56:21 +0000

    Add Cmd to docker_configs

--- base/docker_configs.go
+++ base/docker_configs.go
@@ -4,8 +4,5 @@ type DockerConfigs struct {
 	Image   string
 	Tag     string
 	EnvVars []string
-}
-
-func NewDockerConfigs(image string, tag string, envVars []string) *DockerConfigs {
-	return &DockerConfigs{Image: image, Tag: tag, EnvVars: envVars}
+	Cmd     []string
 }
